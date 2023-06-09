0f4a89422c9760dffdad8f73699a19a6c4dbc0c6
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-06-09 10:12:27 +0600

    create user route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
@@ -12,3 +13,64 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.woapvgk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    //create user
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        //for google signIn // existing user find
+        const query = { email: user.email }
+        const existUser = await usersCollection.findOne(query);
+        if (existUser) {
+          return res.send({ message: 'user already exists' })
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      })
+
+      
+    //jwt
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({ token })
+      })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('music is playing')
+})
+
+app.listen(port, () => {
+    console.log(`Music is playing on port ${port}`);
+})
