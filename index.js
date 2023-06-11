bbfebe9946c562fc254478686a6ae080545da1f4
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-06-11 10:49:10 +0600

    update user to admin or instructor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
@@ -12,6 +12,24 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  //bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+
+  })
+}
 
 
 
@@ -34,29 +52,77 @@ async function run() {
     await client.connect();
 
     const usersCollection = client.db("umusicDb").collection("users");
-    
+    const classCollection = client.db("umusicDb").collection("classes");
 
+    //get all users from mongoDb and send to client
+    app.get('/users', verifyJWT, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
 
-    //create user
+    //create user send to mongoDb and get from client
     app.post('/users', async (req, res) => {
-        const user = req.body;
-        //for google signIn // existing user find
-        const query = { email: user.email }
-        const existUser = await usersCollection.findOne(query);
-        if (existUser) {
-          return res.send({ message: 'user already exists' })
-        }
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      })
+      const user = req.body;
+      //for google signIn // existing user find
+      const query = { email: user.email }
+      const existUser = await usersCollection.findOne(query);
+      if (existUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
 
-      
+    //add class by instructor // send to mongoDb and get from client
+    app.post('/addClassByIns', async (req, res) => {
+      const classData = req.body;
+      const result = await classCollection.insertOne(classData);
+      res.send(result);
+    })
+
+    // update user  to admin
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+    //update user to instructor
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+    //delete user 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+
+    })
+
     //jwt
     app.post('/jwt', (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        res.send({ token })
-      })
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token })
+    })
 
 
     // Send a ping to confirm a successful connection
@@ -71,9 +137,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('music is playing')
+  res.send('music is playing')
 })
 
 app.listen(port, () => {
-    console.log(`Music is playing on port ${port}`);
+  console.log(`Music is playing on port ${port}`);
 })
